3fdf8d4f071f5fe8645118eec680c7b672eca687
ADAM Damien <damien.adam@stagiairesmns.fr>
2023-06-19 11:23:52 +0200

    Upload Angular LOCPARCMNS

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CDA2023
+# ProjetLocMNS
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CDA2023'`, () => {
+  it(`should have as title 'ProjetLocMNS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CDA2023');
+    expect(app.title).toEqual('ProjetLocMNS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CDA2023 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ProjetLocMNS app is running!');
   });
 });
